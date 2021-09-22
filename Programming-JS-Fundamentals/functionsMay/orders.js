84453ff394a5fc1ecb603b01e18588b2b21a8861
Nikolay Dimitrov <90197772+nikolaisl92@users.noreply.github.com>
2021-09-22 19:04:42 +0300

    Update orders.js

--- Programming-JS-Fundamentals/functionsMay/orders.js
+++ Programming-JS-Fundamentals/functionsMay/orders.js
@@ -1,12 +1,12 @@
 function orders(drink, n) {
   let result = 0;
-  if (drink === "coffee") {
+  if (drink == "coffee") {
     result = n * 1.5;
-  } else if (drink === "water") {
+  } else if (drink == "water") {
     result = 1 * n;
-  } else if (drink === "coke") {
+  } else if (drink == "coke") {
     result = 1.4 * n;
-  } else if (drink === "snacks") {
+  } else if (drink == "snacks") {
     result = 2 * n;
   }
   console.log(`${result.toFixed(2)}`);
