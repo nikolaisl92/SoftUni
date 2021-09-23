2d973a6eacad79784a3df1638daf09511619c476
Nikolay Dimitrov <90197772+nikolaisl92@users.noreply.github.com>
2021-09-23 20:43:23 +0300

    Update areaOfFigures.js

--- Programming-JS-Basic/conditionalStatements/areaOfFigures.js
+++ Programming-JS-Basic/conditionalStatements/areaOfFigures.js
@@ -1,12 +1,12 @@
 function areaOfFigures (input) {
     let type = (input[0]);
     if (type === 'square') {
-        let num = Number (input[1]);
+        let num = Number(input[1]);
         let area = num * num;
         console.log(area.toFixed(3))
     } else if (type === 'rectangle') {
-        let a = Number (input[1]);
-        let b = Number (input [2]);
+        let a = Number(input[1]);
+        let b = Number(input [2]);
         let area = a * b;
         console.log(area.toFixed(3))
     } else if (type === 'circle') {
@@ -14,9 +14,9 @@ function areaOfFigures (input) {
         let area = Math.PI * r * r;
         console.log(area.toFixed(3))
     } else {
-        let a = Number (input[1]);
-        let b = Number (input[2]);
-        let area = a * b / 2
+        let a = Number(input[1]);
+        let b = Number(input[2]);
+        let area = (a * b) / 2
         console.log(area.toFixed(3));
     }
     
