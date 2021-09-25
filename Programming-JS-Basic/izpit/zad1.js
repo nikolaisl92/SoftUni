2b8087b2105b2ca3f1eb1c4a7f8b44f1dbbdcfab
Nikolay Dimitrov <90197772+nikolaisl92@users.noreply.github.com>
2021-09-25 06:18:56 -0700

    Update zad1.js

--- Programming-JS-Basic/izpit/zad1.js
+++ Programming-JS-Basic/izpit/zad1.js
@@ -1,13 +1,13 @@
 function solve(input) {
     let videoCard = Number(input[0]);
     let prehodnik = Number(input[1]);
-    let tok = Number(input[2]);
+    let el = Number(input[2]);
     let profit = Number(input[3]);
 
     let allCards = videoCard * 13;
     let allPrehodnici = prehodnik * 13;
     let poharcheno = allCards + allPrehodnici + 1000;
-    let profitDay = profit - tok;
+    let profitDay = profit - el;
     let profitCard = 13 * profitDay;
     let vrushtane = poharcheno / profitCard;
 
