2ef004cb680616087c6024861f2f4b70dcd496da
Nikolay Dimitrov <90197772+nikolaisl92@users.noreply.github.com>
2021-10-03 21:45:43 +0300

    Update characterSequence.js

--- Programming-JS-Basic/forLoopLab/characterSequence.js
+++ Programming-JS-Basic/forLoopLab/characterSequence.js
@@ -1,12 +1,6 @@
 function demo(input) {
 
     let text = input[0];
-    for (let i = 0; i < text.length; i ++) {
-        // let letter = text[i];
-        // console.log(letter)
-
-        // console.log(text[i])
-
         console.log(text.charAt(i))
     }
 }
